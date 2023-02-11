048a4e395e1f86aa154e3bb9cec8bfa4279221a5
HerikFelipe <herik.f.veroneze@gmail.com>
2023-02-11 09:29:36 -0300

    Add livro harry potter

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,3 @@
 INSERT INTO livro (titulo, paginas, autor) VALUES ('O senhor dos anéis', 1000, 'Tolkien');
 INSERT INTO livro (titulo, paginas, autor) VALUES ('Spring Boot', 328, 'Mark Heckler');
+INSERT INTO livro (titulo, paginas, autor) VALUES ('Harry Potter Enigma do Principe', 328, 'JK rowling')
